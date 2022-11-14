bb66a573a9f3d4ca20a48c88c0476774f7564d22
Erickson <36509173+ericksonvergara@users.noreply.github.com>
2022-11-13 19:54:13 -0500

    MERN

--- backend/conexion.js
+++ backend/conexion.js
@@ -3,7 +3,7 @@ mongoose.connect('mongodb://localhost:27017/crudmernstack');
 
 const objetobd = mongoose.connection
 
-//Mensajes de confirmacion
+//Mensajes de confirmacion.
 objetobd.on('connected', ()=>{console.log('Conexion correcta a MongoDB')})
 objetobd.on('error', ()=>{console.log('Error en la conexion a MongoDB')})
 
